036c9619d30d1b2ce2ceb6c9cd8ef60f0e0aeebf
al-imam <alimam01828@gmail.com>
2023-03-25 01:58:08 +0600

    extend tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#6169ff33",
+        light: "#809dff",
+      },
+    },
   },
   plugins: [],
 };
