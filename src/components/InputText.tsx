45a3cd01c79f166ee453b3b4f4e77014b77a8973
al-imam <alimam01828@gmail.com>
2023-03-24 20:32:25 +0600

    create InputText utility component

---
+++ src/components/InputText.tsx
@@ -0,0 +1,29 @@
+interface InputProps {
+  type: string;
+  placeholder: string;
+  dispatch: (value: string) => void;
+  value: string;
+  ac?: string;
+}
+
+const Input: React.FunctionComponent<InputProps> = ({
+  type = "text",
+  placeholder,
+  dispatch,
+  value,
+  ac,
+}) => (
+  <input
+    value={value}
+    onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+      dispatch(evt.currentTarget.value)
+    }
+    className=""
+    type={type}
+    placeholder={placeholder}
+    autoComplete={ac}
+    required
+  />
+);
+
+export default Input;
