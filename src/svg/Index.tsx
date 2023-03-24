0bfb8f1a2c550a43408727af4579f7a285f46c55
al-imam <alimam01828@gmail.com>
2023-03-24 20:45:57 +0600

    add close eye

--- src/svg/Index.tsx
+++ src/svg/Index.tsx
@@ -19,3 +19,26 @@ export function EyeClose(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function Eye(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M10.5 16c3.13 0 5.963-1.833 8.5-5.5C16.463 6.833 13.63 5 10.5 5S4.537 6.833 2 10.5c2.537 3.667 5.37 5.5 8.5 5.5z"></path>
+        <path d="M10.5 7c.185 0 .366.014.543.042a2.5 2.5 0 0 0 2.915 2.916A3.5 3.5 0 1 1 10.5 7z"></path>
+      </g>
+    </svg>
+  );
+}
