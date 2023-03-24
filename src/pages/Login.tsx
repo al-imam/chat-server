3c23a6c17fd7ff8f912a1673f35604be749ff53c
al-imam <alimam01828@gmail.com>
2023-03-24 21:20:10 +0600

    show utility component in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,11 @@
+import InputPassword from "@app/components/InputPassword";
+import InputText from "@app/components/InputText";
+
 export default function Login() {
-  return <form></form>;
+  return (
+    <form>
+      <InputText placeholder="Email" type="email" />
+      <InputPassword placeholder="Password" />
+    </form>
+  );
 }
