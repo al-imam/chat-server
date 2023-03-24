52cb7f826285108e49871068bbff013886546137
al-imam <alimam01828@gmail.com>
2023-03-24 21:29:56 +0600

    style form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import InputText from "@app/components/InputText";
 
 export default function Login() {
   return (
-    <form>
+    <form className="flex flex-col gap-4 w-sm-screen sm:w-[30rem] mx-auto bg-white rounded shadow-sm overflow-hidden px-4 py-8">
       <InputText placeholder="Email" type="email" />
       <InputPassword placeholder="Password" />
     </form>
