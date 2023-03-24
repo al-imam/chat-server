57e3bb3baba940c06efba4d9f4eb7af0f1d13162
al-imam <alimam01828@gmail.com>
2023-03-24 20:51:08 +0600

    create InputPassword

--- src/components/InputPassword.tsx
+++ src/components/InputPassword.tsx
@@ -1,3 +1,35 @@
 import { InputProps } from "@components/InputText";
+import { EyeClose, Eye } from "@svg/Index";
+import { useState } from "react";
 
 type InputPasswordProps = Omit<InputProps, "type">;
+
+const InputPassword: React.FunctionComponent<InputPasswordProps> = ({
+  placeholder,
+  dispatch,
+  value,
+  ac,
+}) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="group">
+      <input
+        className="inputs password"
+        value={value}
+        onChange={(evt: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(evt.currentTarget.value)
+        }
+        type={open ? "text" : "password"}
+        placeholder={placeholder}
+        autoComplete={ac}
+        required
+      />
+      <button className="seen" type="button" onClick={() => setOpen((p) => !p)}>
+        <div className="eye">{open ? <Eye /> : <EyeClose />}</div>
+      </button>
+    </div>
+  );
+};
+
+export default InputPassword;
