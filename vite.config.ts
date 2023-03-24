886337421411fb1e83e428fed91bd5817f7a6c88
al-imam <alimam01828@gmail.com>
2023-03-24 20:39:23 +0600

    add svg folder to path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       "@app": path.resolve(__dirname, "src"),
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@svg": path.resolve(__dirname, "src/svg"),
     },
   },
 });
