1b3e8522a4d6c3fd603bbc46e1a8cad2e9d6f874
al-imam <alimam01828@gmail.com>
2023-03-26 01:47:48 +0600

    style button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,5 +4,12 @@ interface ButtonProps {
 }
 
 export default function Button({ type = "submit", child }: ButtonProps) {
-  return <button type={type}>{child}</button>;
+  return (
+    <button
+      type={type}
+      className="py-3 font-bold text-white rounded bg-my-primary enabled:hover:bg-my-hover enabled:active:bg-my-active disabled:bg-opacity-50 "
+    >
+      {child}
+    </button>
+  );
 }
