addde7c20129b43f7f9739c416681f667baf4e36
al-imam <alimam01828@gmail.com>
2023-03-26 01:16:07 +0600

    create button component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import InputPassword from "@app/components/InputPassword";
 import InputText from "@app/components/InputText";
+import Button from "@app/components/Button";
 
 export default function Login() {
   return (
     <form className="flex flex-col gap-4 w-sm-screen sm:w-[30rem] mx-auto bg-[#fff]  bg-opacity-50 backdrop-blur-md rounded shadow-sm overflow-hidden px-6 py-12">
       <InputText placeholder="Email" type="email" />
       <InputPassword placeholder="Password" />
+      <Button />
     </form>
   );
 }
