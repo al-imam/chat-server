c55d82aa8658d2a9b6091a249b449334d85daf6c
al-imam <alimam01828@gmail.com>
2023-03-26 00:56:14 +0600

    tailwindcss extend font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         light: "#6169ff33",
         dark: "#bac9ff",
       },
+      fontFamily: {
+        primary: "'Ubuntu', sans-serif",
+      },
     },
   },
   plugins: [],
