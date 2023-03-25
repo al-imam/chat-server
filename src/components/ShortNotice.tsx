80bc1c5e4fb239c8cb343423969f6a23265a7184
al-imam <alimam01828@gmail.com>
2023-03-26 02:24:38 +0600

    style shortNotice component

--- src/components/ShortNotice.tsx
+++ src/components/ShortNotice.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function ShortNotice() {
   return (
-    <div>
-      Don't have account? <Link to="singup">Singup</Link>
+    <div className="text-base text-center text-my-slate-800">
+      Don't have account?{" "}
+      <Link to="/singup" className="text-my-primary hover:underline">
+        Singup
+      </Link>
     </div>
   );
 }
