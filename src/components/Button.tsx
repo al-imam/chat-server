6e67bf7ee17850ad5e9b7d73c2bc3305dbc484ec
al-imam <alimam01828@gmail.com>
2023-03-26 01:50:24 +0600

    style disabled state and add disabled props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,19 @@
 interface ButtonProps {
   type?: "submit" | "button" | "reset";
   child: React.ReactNode;
+  disabled?: boolean;
 }
 
-export default function Button({ type = "submit", child }: ButtonProps) {
+export default function Button({
+  type = "submit",
+  child,
+  disabled = false,
+}: ButtonProps) {
   return (
     <button
       type={type}
-      className="py-3 font-bold text-white rounded bg-my-primary enabled:hover:bg-my-hover enabled:active:bg-my-active disabled:bg-opacity-50 "
+      disabled={disabled}
+      className="py-3 font-bold text-white rounded bg-my-primary enabled:hover:bg-my-hover enabled:active:bg-my-active disabled:bg-opacity-50 disabled:cursor-wait "
     >
       {child}
     </button>
