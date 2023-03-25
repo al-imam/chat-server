c8439ddfe82953c6fae6a6d908085e65de1be062
al-imam <alimam01828@gmail.com>
2023-03-26 01:44:12 +0600

    tailwindcss extend color prefix with my

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
       colors: {
         light: "#6169ff33",
         dark: "#bac9ff",
+        my: {
+          primary: "hsl(341deg 66% 67%)",
+          hover: "hsl(341deg 66% 60%)",
+          active: "hsl(335deg 51% 33%)",
+        },
       },
       fontFamily: {
         primary: ["Ubuntu", ...fontFamily.sans],
