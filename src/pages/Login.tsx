53f4e6b4e4d12bbd757afca2101844056cab1919
al-imam <alimam01828@gmail.com>
2023-03-27 03:38:55 +0600

    change place holder text

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,8 +6,8 @@ import ShortNotice from "@app/components/ShortNotice";
 export default function Login() {
   return (
     <form className="flex flex-col gap-4 w-sm-screen sm:w-[30rem] mx-auto sm:bg-[rgb(163,163,163)] mt-16  sm:bg-opacity-5 sm:backdrop-blur-3xl rounded sm:shadow-sm sm:px-6 py-12">
-      <InputText placeholder="Email" type="email" />
-      <InputPassword placeholder="Password" />
+      <InputText placeholder="example@gmail.com" type="email" />
+      <InputPassword placeholder="$^#23_zqx" />
       <Button child="Login" />
       <hr className="text-my-slate-200" />
       <ShortNotice />
