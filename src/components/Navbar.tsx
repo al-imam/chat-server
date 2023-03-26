6cfcf02d9d38aa9b0fa1b7f69eff1c3bba33c73d
al-imam <alimam01828@gmail.com>
2023-03-27 02:09:26 +0600

    style nav logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom";
+import { MessageIcon } from "@svg/Index";
 
 export default function Navbar() {
   return (
     <nav className="flex items-center h-16 bg-black bg-opacity-10 backdrop-blur-lg">
       <ul className="mx-auto w-[calc(100vw-3rem)] sm:max-w-3xl flex ">
         <li className="mr-auto">
-          <NavLink to="/home">Chat server</NavLink>
+          <NavLink to="/home" className="text-my-primary">
+            <MessageIcon />
+          </NavLink>
         </li>
         <li>
           <button>✨</button>
