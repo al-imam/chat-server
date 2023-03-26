008f266776264fb1a2130a7a1f7364722334f0fb
al-imam <alimam01828@gmail.com>
2023-03-27 03:04:00 +0600

    style logo and theme switcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,16 @@ export default function Navbar() {
     <nav className="flex items-center h-16 bg-black bg-opacity-10 backdrop-blur-lg">
       <ul className="mx-auto w-[calc(100vw-3rem)] sm:max-w-3xl flex gap-4 ">
         <li className="mr-auto">
-          <NavLink to="/home" className="text-my-primary  max-h-[2rem]">
+          <NavLink
+            to="/home"
+            className="text-my-primary max-h-[2rem] shadow focus-visible:outline-dark focus-visible:shadow"
+          >
             <MessageIcon />
           </NavLink>
         </li>
         <li className="max-h-[2rem]">
-          <button className="max-h-[2rem]">
-            <DarkMode />
+          <button className="max-h-[2rem] rounded text-my-slate-800 outline-none border-none outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light">
+            <DarkMode className="scale-90" />
           </button>
         </li>
         <li>
