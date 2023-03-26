aebad77649cf694803ea5814d69c481dd08d2a42
al-imam <alimam01828@gmail.com>
2023-03-27 01:48:51 +0600

    change app.tsx structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatServer from "@pages/ChatServer";
 import Login from "@pages/Login";
 import SingUp from "@pages/SingUp";
+import Navbar from "@components/Navbar";
 
 function App() {
   return (
-    <main className="mx-auto sm:max-w-3xl">
-      <BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <main className="mx-auto sm:max-w-3xl">
         <Routes>
           <Route path="/" element={<ChatServer />} />
           <Route path="/login" element={<Login />} />
           <Route path="/singup" element={<SingUp />} />
         </Routes>
-      </BrowserRouter>
-    </main>
+      </main>
+    </BrowserRouter>
   );
 }
 
