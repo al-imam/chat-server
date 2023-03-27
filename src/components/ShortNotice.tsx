fe6bc7cfe96cf36a1276c0a38197fb0c5b3a665f
al-imam <alimam01828@gmail.com>
2023-03-27 19:12:24 +0600

    make shortNotice component reusable

--- src/components/ShortNotice.tsx
+++ src/components/ShortNotice.tsx
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 
-export default function ShortNotice() {
+interface ShortNoticeProps {
+  description: string;
+  to: string;
+  text: string;
+}
+
+export default function ShortNotice({
+  description,
+  to,
+  text,
+}: ShortNoticeProps) {
   return (
     <div className="text-base text-center text-my-slate-800">
-      Don't have account?{" "}
-      <Link to="/singup" className="text-my-primary hover:underline">
-        Singup
+      {description}
+      <Link to={to} className="text-my-primary hover:underline">
+        {text}
       </Link>
     </div>
   );
