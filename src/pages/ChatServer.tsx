fb3db726de59e9ef3b017956a6ce912a9eb4f52f
al-imam <alimam01828@gmail.com>
2023-03-28 02:53:37 +0600

    add form handler

--- src/pages/ChatServer.tsx
+++ src/pages/ChatServer.tsx
@@ -1,11 +1,16 @@
 import SendMessage from "@app/components/SendMessage";
+import { FormEvent } from "react";
 
 export default function ChatServer() {
+  function sendMessage(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+  }
+
   return (
     <div
       className={`mx-auto w-[calc(100vw-3rem)] flex flex-col sm:max-w-3xl  py-4 supports-[height:100dvh]:h-[calc(100dvh-64px)] h-[calc(100vh-64px)] landscape:h-[calc(100vh-64px)] `}
     >
-      <form className="mt-auto">
+      <form onSubmit={sendMessage} className="mt-auto" noValidate={true}>
         <SendMessage />
       </form>
     </div>
