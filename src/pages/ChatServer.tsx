16f195b743385e966fbc404e9f9446f2586bfc9c
al-imam <alimam01828@gmail.com>
2023-03-28 01:59:58 +0600

    track focusing state

--- src/pages/ChatServer.tsx
+++ src/pages/ChatServer.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { Eye } from "@svg/Index";
+
 export default function ChatServer() {
+  const [isFocus, setIsFocus] = useState(false);
   return (
     <div className="mx-auto w-[calc(100vw-3rem)] sm:max-w-3xl bg-red-900 h-[calc(100vh-64px)] supports-[height:100dvh]:h-[calc(100dvh-64px)]">
       Hi chat server
