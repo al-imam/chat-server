cc7227d63d4455687c603a63adbeabdcebc1b6bc
al-imam <alimam01828@gmail.com>
2023-03-28 02:44:55 +0600

    add dvh fallback

--- src/pages/ChatServer.tsx
+++ src/pages/ChatServer.tsx
@@ -5,7 +5,7 @@ export default function ChatServer() {
   const [isFocus, setIsFocus] = useState(false);
   return (
     <div
-      className={`mx-auto w-[calc(100vw-3rem)] flex flex-col sm:max-w-3xl  py-4 h-[calc(100dvh-64px)] landscape:h-[calc(100vh-64px)] `}
+      className={`mx-auto w-[calc(100vw-3rem)] flex flex-col sm:max-w-3xl  py-4 supports-[height:100dvh]:h-[calc(100dvh-64px)] h-[calc(100vh-64px)] landscape:h-[calc(100vh-64px)] `}
     >
       <form className="mt-auto">
         <div
