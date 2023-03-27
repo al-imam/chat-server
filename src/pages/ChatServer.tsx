1b543b99723b44cab9aff4532e74eea02e7c5c7b
al-imam <alimam01828@gmail.com>
2023-03-27 20:05:38 +0600

    make chat view full width

--- src/pages/ChatServer.tsx
+++ src/pages/ChatServer.tsx
@@ -1,3 +1,7 @@
 export default function ChatServer() {
-  return <div>Hi chat server</div>;
+  return (
+    <div className="mx-auto w-[calc(100vw-3rem)] sm:max-w-3xl bg-red-900 h-[calc(100vh-64px)] supports-[height:100dvh]:h-[calc(100dvh-64px)]">
+      Hi chat server
+    </div>
+  );
 }
