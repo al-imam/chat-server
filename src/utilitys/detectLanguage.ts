c724cbdb7ba7e289829d02d0b873835d2dfbeed8
al-imam <alimam01828@gmail.com>
2023-03-29 01:12:00 +0600

    export function

--- src/utilitys/detectLanguage.ts
+++ src/utilitys/detectLanguage.ts
@@ -1,19 +1,11 @@
 //read input value, and remove "space" by replace \s
 //Dictionary for Unicode range of the languages
 const lanCode = {
-  arabic: /[\u0600-\u06FF]/,
-  persian: /[\u0750-\u077F]/,
-  Hebrew: /[\u0590-\u05FF]/,
-  Syriac: /[\u0700-\u074F]/,
-  Bengali: /[\u0980-\u09FF]/,
-  Ethiopic: /[\u1200-\u137F]/,
-  "Greek and Coptic": /[\u0370-\u03FF]/,
-  Georgian: /[\u10A0-\u10FF]/,
-  Thai: /[\u0E00-\u0E7F]/,
+  bangla: /[\u0980-\u09FF]/,
   english: /^[a-zA-Z]+$/,
 } as const;
 
-function lngType(text: string): keyof typeof lanCode | null {
+export default function lngType(text: string): keyof typeof lanCode | null {
   const entries = Object.entries(lanCode);
   for (const [key, value] of entries) {
     if (value.test(text) == true) {
