9b81cf4cedb7c2fa28474a3e5f0a11ad288edea3
al-imam <alimam01828@gmail.com>
2023-03-28 16:57:49 +0600

    complete scrollbar styling

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -5,7 +5,7 @@ export default function Message({ message = { photoURL: null, text: null } }) {
 
   return (
     <div
-      className={`w-[calc(100vw-3rem)] sm:max-w-3xl mx-auto flex gap-2 ${
+      className={`w-[calc(100vw-3rem)] sm:max-w-3xl mx-auto relative flex gap-2 translate-x-1 ${
         rand > 0.5 ? "items-start" : "flex-row-reverse items-end"
       }`}
     >
