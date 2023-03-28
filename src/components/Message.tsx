41bf546a64e9727eb1c5879093c09aeca8e676c4
al-imam <alimam01828@gmail.com>
2023-03-29 00:40:45 +0600

    decrease receive side profile on sm screen

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,7 +14,7 @@ export default function Message({ message = { photoURL: null, text: null } }) {
     >
       {(user || smell) && (
         <img
-          className="w-10 bg-red-200 rounded-full aspect-square"
+          className="w-8 bg-red-200 rounded-full sm:w-10 aspect-square"
           src={
             photoURL ||
             "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
