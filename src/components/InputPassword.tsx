30b18c41285d0e1968a11a96b8b55e6ae5555272
al-imam <alimam01828@gmail.com>
2023-03-30 03:27:16 +0600

    add proper types

--- src/components/InputPassword.tsx
+++ src/components/InputPassword.tsx
@@ -4,10 +4,15 @@ import { useState } from "react";
 
 type InputPasswordProps = Omit<InputProps, "type">;
 
-const InputPassword: React.FunctionComponent<InputPasswordProps> = ({
-  placeholder,
-  disabled = false,
-}) => {
+const InputPassword: React.FunctionComponent<
+  Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "className" | "required" | "onBlur" | "onFocus" | "autoComplete" | "type"
+  >
+> = ({ ...rest }) => {
   const [open, setOpen] = useState(false);
   const [isFocused, setIsFocus] = useState(false);
 
@@ -16,7 +21,7 @@ const InputPassword: React.FunctionComponent<InputPasswordProps> = ({
       className={`flex items-center bg-white rounded outline outline-1  group ${
         isFocused
           ? "outline-dark ring-4 ring-light"
-          : disabled
+          : rest.disabled
           ? "filter-opacity outline-none pointer-events-none"
           : "outline-my-slate-200"
       }`}
@@ -24,12 +29,11 @@ const InputPassword: React.FunctionComponent<InputPasswordProps> = ({
       <input
         className="w-full p-4 bg-white border-none rounded outline-none text-my-slate-800 outline-offset-0 caret-current placeholder:text-my-slate-300 placeholder:select-none password"
         type={open ? "text" : "password"}
-        placeholder={placeholder}
         autoComplete="off"
         required
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
-        disabled={disabled}
+        {...rest}
       />
       <button
         className="relative flex items-center justify-center h-8 mx-2 bg-black bg-opacity-0 border-none rounded-full outline-none aspect-square hover:bg-opacity-5 active:bg-opacity-10 focus-visible:bg-opacity-10"
--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -4,18 +4,20 @@ export interface InputProps {
   disabled?: boolean;
 }
 
-const InputText: React.FunctionComponent<InputProps> = ({
-  type = "text",
-  placeholder,
-  disabled = false,
-}) => (
+const InputText: React.FunctionComponent<
+  Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "className" | "required"
+  >
+> = ({ ...rest }) => (
   <input
     className="p-4 bg-white border-none rounded outline-none text-my-slate-700 outline-offset-0 caret-current placeholder:text-my-slate-300 placeholder:select-none outline-1 enabled:outline-my-slate-200 enabled:focus:outline-dark focus:ring-4 focus:ring-light disabled:bg-opacity-70 text"
-    type={type}
-    placeholder={placeholder}
     autoComplete="off"
     required
-    disabled={disabled}
+    {...rest}
   />
 );
 
