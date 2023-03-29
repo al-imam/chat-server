0a14aceb778deded58550b701f819197458702b4
al-imam <alimam01828@gmail.com>
2023-03-30 03:13:37 +0600

    rename SingUp to Singup

--- src/pages/Singup.tsx
+++ src/pages/Singup.tsx
@@ -3,11 +3,11 @@ import InputText from "@app/components/InputText";
 import Button from "@app/components/Button";
 import ShortNotice from "@app/components/ShortNotice";
 
-export default function SingUp() {
+export default function Singup() {
   return (
-    <section className="w-sm-screen sm:w-[30rem] mx-auto sm:bg-my-primary mt-16  sm:bg-opacity-5 sm:backdrop-blur-3xl rounded sm:shadow-sm sm:px-6 sm:py-12">
+    <section className="mx-auto mt-16 rounded w-sm-screen sm:w-[30rem] sm:bg-my-primary sm:bg-opacity-5 sm:backdrop-blur-3xl sm:shadow-sm sm:px-6 sm:py-12">
       <h1 className="text-4xl sm:mb-12 text-my-slate-800">Singup</h1>
-      <form className="flex flex-col gap-4 w-sm-screen sm:w-[30rem] mx-auto sm:bg-black sm:bg-opacity-5 sm:backdrop-blur-3xl rounded sm:shadow-sm sm:px-6 py-12">
+      <form className="flex flex-col gap-4 py-12 mx-auto rounded w-sm-screen sm:w-[30rem] sm:bg-black sm:bg-opacity-5 sm:backdrop-blur-3xl sm:shadow-sm sm:px-6">
         <InputText placeholder="example@gmail.com" type="email" />
         <InputPassword placeholder="$^#23_zqx" />
         <InputPassword placeholder="retype - $^#23_zqx" />
