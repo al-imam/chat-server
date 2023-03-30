112c0e1a04e75cc1758d910be070c7b885fcde70
al-imam <alimam01828@gmail.com>
2023-03-31 03:14:42 +0600

    copy complex ts type AtLeastOne from stack overflow

--- src/hooks/useStore.tsx
+++ src/hooks/useStore.tsx
@@ -1,8 +1,11 @@
 import { useReducer } from "react";
 
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
+  U[keyof U];
+
 export default function useStore<StoreType>(
   init: StoreType
-): [StoreType, React.Dispatch<Partial<StoreType>>] {
+): [StoreType, React.Dispatch<AtLeastOne<StoreType>>] {
   return useReducer(
     (prev: StoreType, next: Partial<StoreType>) => ({ ...prev, ...next }),
     init
