5c5995f8d9f1fe90999af7ee0f99c3cf26de4548
al-imam <alimam01828@gmail.com>
2023-03-30 16:40:26 +0600

    style Button component focus-visible state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ export default function Button({
     <button
       type={type}
       disabled={disabled}
-      className="py-3 font-bold text-white rounded enabled:active:text-my-slate-300 bg-my-primary enabled:hover:bg-my-hover enabled:active:bg-my-active disabled:bg-opacity-50 disabled:cursor-wait "
+      className="py-3 font-bold text-white rounded outline-none enabled:active:text-my-slate-300 bg-my-primary enabled:hover:bg-my-hover enabled:active:bg-my-active disabled:bg-opacity-50 disabled:cursor-wait outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light"
     >
       {child}
     </button>
