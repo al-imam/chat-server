39354239e2c56f516affa6e0e61826b2dc56f9a0
al-imam <alimam01828@gmail.com>
2023-03-31 23:54:58 +0600

    take all input element argument as rest

--- src/components/SendMessage.tsx
+++ src/components/SendMessage.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import { SendMessageIcon } from "@svg/Index";
 
-export default function SendMessage({ placeholder = "Send message" }) {
+export default function SendMessage({
+  ...rest
+}: Omit<
+  React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >,
+  "className" | "onBlur" | "onFocus" | "autoComplete" | "required"
+>) {
   const [isFocus, setIsFocus] = useState(false);
   return (
     <div
@@ -11,12 +19,11 @@ export default function SendMessage({ placeholder = "Send message" }) {
     >
       <input
         className="w-full p-4 bg-white border-none rounded outline-none text-my-slate-800 outline-offset-0 caret-current placeholder:text-fg-300 placeholder:select-none password"
-        type="text"
-        placeholder={placeholder}
         autoComplete="off"
         required
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
+        {...rest}
       />
       <button
         className="flex-grow mr-3 rounded-sm outline-none text-primary hover:text-primary-hover active:text-primary-active focus-visible:outline-4 focus-visible:outline-light focus-visible:outline-offset-0"
