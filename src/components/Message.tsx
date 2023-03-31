5ee3e804e00537a22e11d93118a68b872aab10b2
al-imam <alimam01828@gmail.com>
2023-04-01 00:22:04 +0600

    check is message send or received

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,17 +1,22 @@
 import useMedia from "@app/hooks/useMedia";
 import lngType from "@app/utilitys/detectLanguage";
 import { DocumentType } from "@app/hooks/useRealTimeUpdates";
+import useAuth from "@app/hooks/useAuth";
 
 export default function Message({ message, photoURL, uid }: DocumentType) {
   const smell = useMedia("(min-width: 40rem)");
 
+  const { currentUser } = useAuth();
+
+  const send = currentUser?.uid === uid ? false : true;
+
   return (
     <div
       className={`sm-width sm:max-w-3xl mx-auto relative flex gap-2 translate-x-[1px] ${
-        false ? "items-start" : "flex-row-reverse items-end"
+        send ? "items-start" : "flex-row-reverse items-end"
       }`}
     >
-      {(false || smell) && (
+      {(send || smell) && (
         <img
           className="w-8 bg-red-200 rounded-full sm:w-10 aspect-square"
           src={
@@ -22,7 +27,7 @@ export default function Message({ message, photoURL, uid }: DocumentType) {
       )}
       <p
         className={`w-[fit-content] max-w-[65%] md:max-w-[55%] min-h-[40px] flex items-center rounded-md p-2 backdrop-blur-lg ${
-          false
+          send
             ? "bg-black bg-opacity-5 text-fg"
             : "bg-[#0b93f6] bg-opacity-70 text-white"
         } ${lngType(message) === "bangla" ? "font-bangla" : "font-primary"}`}
