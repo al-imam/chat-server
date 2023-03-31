2345f5f924758713d0cc5a035ee2068271813594
al-imam <alimam01828@gmail.com>
2023-04-01 00:40:59 +0600

    guard routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Login from "@app/pages/Login";
 import Singup from "@app/pages/Singup";
 import Navbar from "@app/components/Navbar";
 import { AuthProvider } from "@app/context/AuthContext";
+import Private from "@app/routes/Private";
+import Public from "@app/routes/Public";
 
 function App() {
   return (
@@ -12,9 +14,18 @@ function App() {
         <Navbar />
         <main className="mx-auto ">
           <Routes>
-            <Route path="/" element={<ChatServer />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/singup" element={<Singup />} />
+            <Route
+              path="/"
+              element={<Private to="/login" element={<ChatServer />} />}
+            />
+            <Route
+              path="/login"
+              element={<Public to="/" element={<Login />} />}
+            />
+            <Route
+              path="/singup"
+              element={<Public to="/" element={<Singup />} />}
+            />
           </Routes>
         </main>
       </AuthProvider>
