c9168242dd54219f8dbdb99198b5fe1f17697e66
al-imam <alimam01828@gmail.com>
2023-03-31 23:11:16 +0600

    Fix  navAction component dom Nesting

--- src/components/NavAction.tsx
+++ src/components/NavAction.tsx
@@ -7,14 +7,12 @@ export default function NavAction() {
   return (
     <li>
       {currentUser ? (
-        <li>
-          <button
-            onClick={async () => await logout()}
-            className="flex items-center h-full px-6 py-2 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active"
-          >
-            Logout
-          </button>
-        </li>
+        <button
+          onClick={async () => await logout()}
+          className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active"
+        >
+          Logout
+        </button>
       ) : (
         <NavLink
           className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active"
