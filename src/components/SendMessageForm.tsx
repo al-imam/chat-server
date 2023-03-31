fbdb0819023f8771688a4f2d1eff3efdbe32e4f3
al-imam <alimam01828@gmail.com>
2023-04-01 01:37:12 +0600

    copy past sendMessage component here 😂

--- src/components/SendMessageForm.tsx
+++ src/components/SendMessageForm.tsx
@@ -1,14 +1,15 @@
 import { FormEvent } from "react";
-import SendMessage from "@app/components/SendMessage";
+import { SendMessageIcon } from "@svg/Index";
 import useStore from "@app/hooks/useStore";
 import useCreateNewMessageSetter from "@app/hooks/useCreateNewMessageSetter";
 
 const initialValue = {
   message: "",
+  isFocus: false,
 };
 
 function SendMessageForm() {
-  const [{ message }, updateStore] = useStore(initialValue);
+  const [{ message, isFocus }, updateStore] = useStore(initialValue);
 
   const setNewMessage = useCreateNewMessageSetter("message");
 
@@ -24,13 +25,28 @@ function SendMessageForm() {
       className="mx-auto sm-width sm:max-w-3xl "
       noValidate={true}
     >
-      <SendMessage
-        value={message}
-        onChange={(e) => updateStore({ message: e.target.value })}
-        name="message"
-        placeholder="Send message"
-        type="text"
-      />
+      <div
+        className={`flex items-center bg-white rounded outline outline-1  group ${
+          isFocus ? "outline-dark ring-4 ring-light" : "outline-fg-200"
+        }`}
+      >
+        <input
+          value={message}
+          onChange={(e) => updateStore({ message: e.target.value })}
+          onFocus={() => updateStore({ isFocus: true })}
+          onBlur={() => updateStore({ isFocus: false })}
+          placeholder="Send message"
+          className="w-full p-4 bg-white border-none rounded outline-none text-my-slate-800 outline-offset-0 caret-current placeholder:text-fg-300 placeholder:select-none password"
+          autoComplete="off"
+          required
+        />
+        <button
+          className="flex-grow mr-3 rounded-sm outline-none text-primary hover:text-primary-hover active:text-primary-active focus-visible:outline-4 focus-visible:outline-light focus-visible:outline-offset-0"
+          type="submit"
+        >
+          <SendMessageIcon />
+        </button>
+      </div>
     </form>
   );
 }
