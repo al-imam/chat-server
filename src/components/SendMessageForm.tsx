63e48c53e4fa64b5e068ac0cea64c75d74cd87f2
al-imam <alimam01828@gmail.com>
2023-04-01 01:28:57 +0600

    add message on submit

--- src/components/SendMessageForm.tsx
+++ src/components/SendMessageForm.tsx
@@ -1,12 +1,21 @@
 import { FormEvent } from "react";
 import SendMessage from "@app/components/SendMessage";
 import useStore from "@app/hooks/useStore";
+import useCreateNewMessageSetter from "@app/hooks/useCreateNewMessageSetter";
+
+const initialValue = {
+  message: "",
+};
 
 function SendMessageForm() {
-  const [{ message }, updateStore] = useStore({ message: "" });
+  const [{ message }, updateStore] = useStore(initialValue);
+
+  const setNewMessage = useCreateNewMessageSetter("message");
 
   async function sendMessage(evt: FormEvent<HTMLFormElement>) {
     evt.preventDefault();
+    updateStore(initialValue);
+    await setNewMessage({ message });
   }
 
   return (
