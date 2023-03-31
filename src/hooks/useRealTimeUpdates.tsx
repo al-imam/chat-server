80f55318a51067afc14042956149c91afb7bd4cc
al-imam <alimam01828@gmail.com>
2023-04-01 01:16:08 +0600

    remove unnecessary codes from realtime hook

--- src/hooks/useRealTimeUpdates.tsx
+++ src/hooks/useRealTimeUpdates.tsx
@@ -1,5 +1,4 @@
 import app from "@app/initializeFirebase";
-import useAuth from "@app/hooks/useAuth";
 import { useState, useEffect } from "react";
 import {
   collection,
@@ -8,8 +7,6 @@ import {
   orderBy,
   limitToLast,
   onSnapshot,
-  addDoc,
-  serverTimestamp,
 } from "firebase/firestore";
 
 interface Argument {
@@ -29,46 +26,33 @@ const db = getFirestore(app);
 
 function useRealTimeUpdates({ reference, limit = 30 }: Argument) {
   const [messages, setMessages] = useState<DocumentType[]>([]);
-  const { currentUser } = useAuth();
-
   useEffect(() => {
-    const destroy = onSnapshot(
-      query(
-        collection(db, reference),
-        orderBy("createdAt"),
-        limitToLast(limit)
-      ),
-      (snapshot) => {
-        const updatedData = snapshot.docs.map((snap) => {
-          const { uid, photoURL, createdAt, message } = snap.data();
-
-          return {
-            message,
-            id: snap.id,
-            uid,
-            photoURL,
-            createdAt: new Date(createdAt * 1000),
-          } as DocumentType;
-        });
-        setMessages(updatedData);
-      }
+    const ref = query(
+      collection(db, reference),
+      orderBy("createdAt"),
+      limitToLast(limit)
     );
+
+    const destroy = onSnapshot(ref, (snapshot) => {
+      const updatedData = snapshot.docs.map((snap) => {
+        const { uid, photoURL, createdAt, message } = snap.data();
+
+        return {
+          message,
+          id: snap.id,
+          uid,
+          photoURL,
+          createdAt: new Date(createdAt * 1000),
+        } as DocumentType;
+      });
+
+      setMessages(updatedData);
+    });
 
     return destroy;
   }, [reference]);
 
-  function setNewMessage({ message }: Pick<DocumentType, "message">) {
-    if (currentUser === null) return;
-    const ref = collection(db, reference);
-    return addDoc(ref, {
-      uid: currentUser.uid,
-      message,
-      photoURL: currentUser.photoURL,
-      createdAt: serverTimestamp(),
-    });
-  }
-
-  return { messages, setNewMessage };
+  return messages;
 }
 
 export default useRealTimeUpdates;
