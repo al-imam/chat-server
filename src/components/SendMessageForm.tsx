3ad108c43342cc16bef2f32eb0421730541f69d2
al-imam <alimam01828@gmail.com>
2023-04-01 01:23:50 +0600

    control sendMessage input

--- src/components/SendMessageForm.tsx
+++ src/components/SendMessageForm.tsx
@@ -1,7 +1,10 @@
 import { FormEvent } from "react";
 import SendMessage from "@app/components/SendMessage";
+import useStore from "@app/hooks/useStore";
 
 function SendMessageForm() {
+  const [{ message }, updateStore] = useStore({ message: "" });
+
   async function sendMessage(evt: FormEvent<HTMLFormElement>) {
     evt.preventDefault();
   }
@@ -12,7 +15,13 @@ function SendMessageForm() {
       className="mx-auto sm-width sm:max-w-3xl "
       noValidate={true}
     >
-      <SendMessage name="message" placeholder="Send message" type="text" />
+      <SendMessage
+        value={message}
+        onChange={(e) => updateStore({ message: e.target.value })}
+        name="message"
+        placeholder="Send message"
+        type="text"
+      />
     </form>
   );
 }
