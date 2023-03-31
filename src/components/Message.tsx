613dbd384bdaa11773864511ae9dc5be499f35e9
al-imam <alimam01828@gmail.com>
2023-04-01 00:13:27 +0600

    change Message component to work with real time data

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,25 +1,17 @@
 import useMedia from "@app/hooks/useMedia";
 import lngType from "@app/utilitys/detectLanguage";
+import { DocumentType } from "@app/hooks/useRealTimeUpdates";
 
-export default function Message({ message = { photoURL: null, text: null } }) {
+export default function Message({ message, photoURL, uid }: DocumentType) {
   const smell = useMedia("(min-width: 40rem)");
-  const { photoURL, text } = message;
-
-  const user = Math.random() > 0.5 ? false : true;
-  const t =
-    Math.random() > 0.5
-      ? "Hello guys how are you"
-      : Math.random() > 0.5
-      ? "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis tempore unde provident eaque quibusdam. Vero."
-      : "গুগল এমন একটি নতুন ফিচারে ইনস্টাগ্রামসহ অন্যান্য মাধ্যমের ছোট ছোট ভিডিও দেখা যাবে।";
 
   return (
     <div
       className={`sm-width sm:max-w-3xl mx-auto relative flex gap-2 translate-x-[1px] ${
-        user ? "items-start" : "flex-row-reverse items-end"
+        false ? "items-start" : "flex-row-reverse items-end"
       }`}
     >
-      {(user || smell) && (
+      {(false || smell) && (
         <img
           className="w-8 bg-red-200 rounded-full sm:w-10 aspect-square"
           src={
@@ -30,12 +22,12 @@ export default function Message({ message = { photoURL: null, text: null } }) {
       )}
       <p
         className={`w-[fit-content] max-w-[65%] md:max-w-[55%] min-h-[40px] flex items-center rounded-md p-2 backdrop-blur-lg ${
-          user
+          false
             ? "bg-black bg-opacity-5 text-fg"
             : "bg-[#0b93f6] bg-opacity-70 text-white"
-        } ${lngType(t) === "bangla" ? "font-bangla" : "font-primary"}`}
+        } ${lngType(message) === "bangla" ? "font-bangla" : "font-primary"}`}
       >
-        {t}
+        {message}
       </p>
     </div>
   );
