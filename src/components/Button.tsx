408a5e5d4736c2f17d898192506a0d71024358c5
al-imam <alimam01828@gmail.com>
2023-04-02 02:55:20 +0600

    adapt dark mood in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ export default function Button({
     <button
       type={type}
       disabled={disabled}
-      className="py-3 font-bold text-white rounded outline-none enabled:active:text-fg-300 bg-primary enabled:hover:bg-primary-hover enabled:active:bg-primary-active disabled:bg-primary/50 disabled:cursor-wait outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light"
+      className="py-3 font-bold text-white rounded outline-none enabled:active:text-fg-300 bg-primary enabled:hover:bg-primary-hover enabled:active:bg-primary-active disabled:bg-primary/50 disabled:cursor-wait outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark dark:focus-visible:outline-transparent focus-visible:ring focus-visible:ring-light"
     >
       {child}
     </button>
