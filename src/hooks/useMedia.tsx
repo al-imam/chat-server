0e445027f0f4013de126e94ead44b82226924914
al-imam <alimam01828@gmail.com>
2023-04-02 11:45:26 +0600

    adapt useMedia for every other media query uses

--- src/hooks/useMedia.tsx
+++ src/hooks/useMedia.tsx
@@ -8,9 +8,12 @@ const useMedia = (query: string): boolean => {
     if (media.matches !== matches) {
       setMatches(media.matches);
     }
-    const listener = () => setMatches(media.matches);
-    window.addEventListener("resize", listener);
-    return () => window.removeEventListener("resize", listener);
+    const listener = () => {
+      if (media.matches === matches) return;
+      setMatches(media.matches);
+    };
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
   }, [matches, query]);
 
   return matches;
