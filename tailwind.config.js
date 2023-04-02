05885722655f6a4abb081580bf94005c3bb10349
al-imam <alimam01828@gmail.com>
2023-04-03 03:01:23 +0600

    add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
 const { fontFamily } = require("tailwindcss/defaultTheme");
-const { slate } = require("tailwindcss/colors");
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -31,6 +30,7 @@ export default {
       fontFamily: {
         primary: ["Ubuntu", ...fontFamily.sans],
         bangla: ["Hind Siliguri", ...fontFamily.sans],
+        prime: ["Courier Prime", ...fontFamily.mono],
       },
     },
   },
