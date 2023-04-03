98292d07fe943bcad52432fd3e36b9d0360ed8da
al-imam <alimam01828@gmail.com>
2023-04-03 12:16:23 +0600

    Fix input disabled state bug

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -14,7 +14,7 @@ const InputText: React.FunctionComponent<
   >
 > = ({ ...rest }) => (
   <input
-    className="p-4 bg-white border-none rounded outline-none text-fg outline-offset-0 caret-current placeholder:text-fg-300 placeholder:select-none outline-1 enabled:outline-fg-200 dark:enabled:outline-transparent enabled:focus:outline-dark dark:enabled:focus:outline-transparent focus:ring-4 focus:ring-light disabled:bg-white/70 text"
+    className="p-4 bg-white border-none rounded outline-none text-fg outline-offset-0 caret-current placeholder:text-fg-300 placeholder:select-none outline-1 enabled:outline-fg-200 dark:enabled:outline-transparent enabled:focus:outline-dark dark:enabled:focus:outline-transparent focus:ring-4 focus:ring-light disabled:filter-opacity "
     autoComplete="off"
     required
     {...rest}
