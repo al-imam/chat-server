d4a5465c12ae54062fb93c65b8fa5b3922797804
al-imam <alimam01828@gmail.com>
2023-04-03 13:11:43 +0600

    complete alert style

--- src/components/ErrorAlert.tsx
+++ src/components/ErrorAlert.tsx
@@ -8,11 +8,15 @@ interface AlertProps {
 function ErrorAlert({ error, close }: AlertProps) {
   return (
     <div className="flex items-center gap-2 p-4 bg-red-400 rounded text-slate-900">
-      <div className="flex items-center justify-center ">
+      <div className="flex items-center justify-center scale-75 ">
         <ErrorIcon />
       </div>
       <p className="mr-auto">{error}</p>
-      <button onClick={close} type="button">
+      <button
+        className="flex items-center justify-center rounded-full p-1 outline-none focus-visible:bg-white/30 hover:bg-white/30 scale-125 backdrop-blur-3xl"
+        onClick={close}
+        type="button"
+      >
         <CloseIcon />
       </button>
     </div>
