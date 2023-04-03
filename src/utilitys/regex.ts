70ce0fa6086b9e0fc91349fc28a4272bf6821fc1
al-imam <alimam01828@gmail.com>
2023-04-03 13:23:18 +0600

    copy email regex from my lpi project

---
+++ src/utilitys/regex.ts
@@ -0,0 +1,60 @@
+function startWith(ex: string) {
+  return `^${ex}`;
+}
+
+function endWith(ex: string) {
+  return `${ex}$`;
+}
+
+function letter({ lowerCase = true, upperCase = true, range = "{1,}" } = {}) {
+  return `[${lowerCase ? "a-z" : ""}${upperCase ? "A-Z" : ""}]${range}`;
+}
+
+function word({ underscore = false, haveDot = false } = {}) {
+  if (haveDot)
+    return wordWithHaveDots({ underscore, acceptEndWithNumber: true });
+  return `[a-zA-Z9-0${underscore ? "_" : ""}]`;
+}
+
+function wordWithHaveDots({
+  underscore = false,
+  acceptEndWithNumber = false,
+  range = "{0,}",
+} = {}) {
+  return `(${escape(".")}[a-zA-Z0-9${underscore ? "_" : ""}]+)*${
+    acceptEndWithNumber ? `[a-zA-Z0-9]${range}` : ""
+  }`;
+}
+
+function mustHave(ex: string) {
+  return ex;
+}
+
+function range({ min = 2, max = 5 } = {}) {
+  return `{${min},${max}}`;
+}
+
+function escape(ex: string) {
+  return `\\${ex}`;
+}
+
+const email = new RegExp(
+  startWith(letter()) +
+    word({ haveDot: true }) +
+    mustHave("@") +
+    letter({
+      lowerCase: true,
+      upperCase: false,
+      range: range({ min: 2, max: 10 }),
+    }) +
+    mustHave(escape(".")) +
+    endWith(
+      letter({
+        lowerCase: true,
+        upperCase: false,
+        range: range({ min: 2, max: 7 }),
+      })
+    )
+);
+
+export default email;
