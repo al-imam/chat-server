78c07f4b16f2eeabc4b673f2659082695ccefedb
al-imam <alimam01828@gmail.com>
2023-04-06 03:17:37 +0600

    configure VitePWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,53 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: "fullscreen",
+        name: "chat-server",
+        start_url: "/",
+        lang: "english",
+        description: "chat-server is progressive web app ❤️",
+        background_color: "#0f172a",
+        theme_color: "#0f172a",
+        short_name: "chat-server",
+        categories: ["chat", "communication"],
+        orientation: "portrait",
+        icons: [
+          {
+            src: "/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "src"),
