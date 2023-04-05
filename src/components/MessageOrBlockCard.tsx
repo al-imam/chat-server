2fab8d7c862d1991f9a6a918494b897a85eaaff8
al-imam <alimam01828@gmail.com>
2023-04-05 17:19:20 +0600

    when photoURL is null show block card

--- src/components/MessageOrBlockCard.tsx
+++ src/components/MessageOrBlockCard.tsx
@@ -1,11 +1,8 @@
 import { DocumentType } from "@app/hooks/useRealTimeUpdates";
 import Message from "./Message";
 
-function MessageOrBlockCard({
-  block = Math.random() > 0.5 ? true : false,
-  ...rest
-}: DocumentType & { block?: boolean }) {
-  return block ? (
+function MessageOrBlockCard({ ...rest }: DocumentType & { block?: boolean }) {
+  return rest.photoURL === null ? (
     <p className="mx-auto text-sm text-center sm:text-base sm-width-block sm:max-w-3xl text-slate-600 dark:text-slate-400 ">
       {rest.email} blocked for breaking community guideline!
     </p>
