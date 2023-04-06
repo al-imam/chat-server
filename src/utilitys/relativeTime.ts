1921cf77810328261d8ed874fdac3df379813585
al-imam <alimam01828@gmail.com>
2023-04-07 01:19:42 +0600

    update relativeTimeFormatter function
      - when number is singular show singular text
      - create describeTime function to refine text

--- src/utilitys/relativeTime.ts
+++ src/utilitys/relativeTime.ts
@@ -1,18 +1,22 @@
 function relativeTimeFormatter(date: Date) {
   const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
   if (seconds < 60) {
-    return `${seconds} seconds ago`;
+    return describeTime(seconds, "second", "seconds");
   } else if (seconds < 3600) {
-    return `${Math.floor(seconds / 60)} minutes ago`;
+    return describeTime(Math.floor(seconds / 60), "minute", "minutes");
   } else if (seconds < 86400) {
-    return `${Math.floor(seconds / 3600)} hours ago`;
+    return describeTime(Math.floor(seconds / 3600), "hour", "hours");
   } else if (seconds < 2620800) {
-    return `${Math.floor(seconds / 86400)} days ago`;
+    return describeTime(Math.floor(seconds / 86400), "day", "days");
   } else if (seconds < 31449600) {
-    return `${Math.floor(seconds / 2620800)} months ago`;
+    return describeTime(Math.floor(seconds / 2620800), "month", "months");
   } else {
-    return `${Math.floor(seconds / 31449600)} years ago`;
+    return describeTime(Math.floor(seconds / 31449600), "year", "years");
   }
+}
+
+function describeTime(time: number, singular: string, plural: string) {
+  return `${time} ${time === 1 ? singular : plural} ago`;
 }
 
 export default relativeTimeFormatter;
