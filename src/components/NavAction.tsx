2a82a1740d01b53c2a8dccfbf07492a352a34a59
al-imam <alimam01828@gmail.com>
2023-04-07 00:03:54 +0600

    apply custom drop shadow to NavLink component

--- src/components/NavAction.tsx
+++ src/components/NavAction.tsx
@@ -10,18 +10,18 @@ export default function NavAction() {
       {currentUser ? (
         <button
           onClick={async () => await logout()}
-          className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active dark:focus-visible:outline-none"
+          className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active dark:focus-visible:outline-none "
           aria-label="Logout button"
         >
-          Logout
+          <span className="drop-shadow-cstm">Logout</span>
         </button>
       ) : (
         <NavLink
-          className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active dark:focus-visible:outline-none"
+          className="flex items-center h-full px-6 text-white border-none rounded shadow outline-none max-h-[2rem] bg-primary outline-offset-0 focus-visible:outline-1 focus-visible:outline-dark focus-visible:ring focus-visible:ring-light hover:bg-primary-hover active:bg-primary-active dark:focus-visible:outline-none "
           to={login ? "/singup" : "/login"}
           aria-label={`${login ? "Singup" : "Login"} link`}
         >
-          {login ? "Singup" : "Login"}
+          <span className="drop-shadow-cstm">{login ? "Singup" : "Login"}</span>
         </NavLink>
       )}
     </li>
